9ae5cdd414559745f58638a39e2b7521312ada95
Abhigyan Jaiswal <abhigyanjj@gmail.com>
2021-02-06 12:06:16 +0900

    Added comments to app.py and another route

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+# Imports
+
 import flask
 from flask import request, jsonify, render_template
 
 from chat.chatbot import chat_bot
 
+# Setting up flask app
 app = flask.Flask(__name__)
 
 
+# Adding home route
 @app.route("/", methods=['GET', 'POST'])
+@app.route("/index.html", methods=['GET', 'POST'])
 def home():
+    """Handles home route"""
+
     if request.method == 'POST':
+        # Predict the entered text on the front-end request
         text = chat_bot.predict(text=request.json['text'])
         return jsonify({"data": {"text": text}})
     else:
@@ -16,4 +24,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run()  # Running app
